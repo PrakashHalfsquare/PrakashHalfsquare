acafa575da02a1253045235da80de95fb3992f14
Prakash Pambhar <127112867+PrakashHalfsquare@users.noreply.github.com>
2023-10-28 10:24:10 +0530

    update file

--- ReactJS-Example.jsx
+++ ReactJS-Example.jsx
@@ -44,3 +44,47 @@ const WhiteDoc = require("./white-doc-new.png");
 
 export { BlueCameraVideo };
 <img src={LogoImg} alt="PLAT" className="mx-auto" />;
+
+// Step change
+const [currentStep, setCurrentStep] = useState(1);
+const handleNext = () => {
+  if (currentStep < 4) {
+    setCurrentStep(currentStep + 1);
+  }
+};
+
+const handlePrev = () => {
+  if (currentStep > 1) {
+    setCurrentStep(currentStep - 1);
+  }
+};
+
+<div className="flex gap-4 mt-3 pr-3 pl-1 md:flex-nowrap sm:flex-wrap flex-wrap">
+  <div
+    className={`mx-3 tab w-full  font-semibold  ${
+      currentStep >= 1
+        ? "tab-active text-brand-primary  bg-bg-gray"
+        : "tab-inactive text-text-secondary"
+    }`}
+  >
+    <span className="text-sm">Creating a warm-up webinar</span>
+  </div>
+  <div
+    className={`mx-3  tab w-full  font-semibold  ${
+      currentStep >= 2
+        ? "tab-active text-brand-primary  bg-bg-gray"
+        : "tab-inactive text-text-secondary"
+    }`}
+  >
+    <span className="text-sm">Create a course card for sales</span>
+  </div>
+</div>;
+
+{
+  currentStep === 1 && <Step2 onNext={handleNext} />;
+}
+{
+  currentStep === 2 && <Step2 onNext={handleNext} onPrev={handlePrev} />;
+}
+
+//
